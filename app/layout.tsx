962e5be7e6b111ddbbc1823c3f35be066c049268
Hayden Bleasel <hello@haydenbleasel.com>
2026-02-21 14:15:41 -0800

    Remove unused dependencies, fix Katex

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import { Toaster } from "sonner";
 import { ThemeProvider } from "@/components/theme-provider";
+import "katex/dist/katex.min.css";
 
 import "./globals.css";
 import { SessionProvider } from "next-auth/react";
